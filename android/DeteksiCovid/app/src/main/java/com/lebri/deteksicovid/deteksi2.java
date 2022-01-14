9b4e1036c2da6bdcfc90c88b41f2d6a1a58239b7
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2022-01-14 07:51:33 +0700

    update

---
+++ android/DeteksiCovid/app/src/main/java/com/lebri/deteksicovid/deteksi2.java
@@ -0,0 +1,14 @@
+package com.lebri.deteksicovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class deteksi2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deteksi2);
+    }
+}
---
+++ android/DeteksiCovid/app/src/main/java/com/lebri/deteksicovid/hasil.java
@@ -0,0 +1,14 @@
+package com.lebri.deteksicovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+    }
+}
